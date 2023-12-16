29cf623f234a1740cde779f9b51cd5baffe8f14b
nas <vividnasubi@gmail.com>
2023-12-16 22:34:17 +0900

    feat: userスキーマの定義

---
+++ pkg/domain/model/user.go
@@ -0,0 +1,16 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name        string `json:"name" gorm:"not null"`
+	Email       string `json:"email" gorm:"not null;unique"`
+	PhotoURL    string `json:"photoUrl"`
+	AccountCode string `json:"accountCode"`
+	BankCode    string `gorm:"default:null"`
+	BranchCode  string `json:"branchCode"`
+	// GroupUsers     []GroupUser `gorm:"foreignKey:UserID"`
+	// PaymentsPaidBy []Payment   `gorm:"foreignKey:PaidBy"`
+	// PaymentsPaidTo []Payment   `gorm:"foreignKey:PaidTo"`
+}

92305f8c6c1e3e859cafff31996a7e3b4db1bfae
rom6621 <3103rom@gmail.com>
2023-12-17 09:59:28 +0900

    feat: indexページの実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  Grid,
+  GridItem,
+  Heading,
+  Stack,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      layout: "top",
+    },
+  };
+};
 
 const Home = () => {
   return (
@@ -11,7 +27,27 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Heading>Hello Datti!</Heading>
+        <Grid templateColumns="repeat(12, 1fr)">
+          <GridItem colSpan={12}>
+            <Stack direction="column" pt={180} gap={4} color="white">
+              <Heading size="4xl">Hello Datti!</Heading>
+              <Heading size="lg">誰にいくら払ったっけ？</Heading>
+              <Heading size="lg">を記録するアプリ</Heading>
+            </Stack>
+          </GridItem>
+          <GridItem colSpan={12}>
+            <Center pt={120}>
+              <Button
+                colorScheme="facebook"
+                size="lg"
+                as={Link}
+                href="/dashboard"
+              >
+                はじめる
+              </Button>
+            </Center>
+          </GridItem>
+        </Grid>
       </main>
     </>
   );

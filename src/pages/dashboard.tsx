cb93f4ae82ab5e26cb2d019f444ead89f9e18927
rom6621 <3103rom@gmail.com>
2023-12-17 09:59:51 +0900

    feat: dashboardページの実装

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,34 @@
+import {
+  Button,
+  Center,
+  Grid,
+  GridItem,
+  Heading,
+  Stack,
+} from "@chakra-ui/react";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Datti</title>
+        <meta name="description" content="Datti Web" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Grid templateColumns="repeat(12, 1fr)">
+          <GridItem colSpan={12}>
+            <Heading size="lg" mt={10}>
+              ダッシュボード
+            </Heading>
+          </GridItem>
+        </Grid>
+      </main>
+    </>
+  );
+};
+
+export default Home;

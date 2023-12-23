18ef5236db1210aa80a51a95c13769f6bbd32eca
nas <vividnasubi@gmail.com>
2023-12-23 16:31:49 +0900

    feat: peopleミドルウェアの実装

--- pkg/interface/api/server/server.go
+++ pkg/interface/api/server/server.go
@@ -5,6 +5,7 @@ import (
 	repositoryimpl "github.com/datti-api/pkg/infrastructure/repositoryimpl"
 	"github.com/datti-api/pkg/interface/api/handler"
 	"github.com/datti-api/pkg/usecase"
+	"github.com/datti-api/pkg/utils"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +22,7 @@ func Sever(dsn string) {
 	userHandler := handler.NewUserHandler(userUseCase)
 
 	r := gin.Default()
+	r.Use(utils.PeopleMmiddleware)
 
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{
@@ -36,37 +38,10 @@ func Sever(dsn string) {
 		"GET",
 		"POST",
 		"PUT",
-		"OPTIONS",
 	)
 	config.AllowCredentials = true
 	// cros設定
 	r.Use(cors.New(config))
-	// r.Use(cors.New(cors.Config{
-	// 	// アクセスを許可したいアクセス元
-	// 	AllowOrigins: []string{
-	// 		"http://192.168.10.109:3000",
-	// 	},
-	// 	// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
-	// 	AllowMethods: []string{
-	// 		"POST",
-	// 		"GET",
-	// 		"PUT",
-	// 	},
-	// 	// 許可したいHTTPリクエストヘッダ
-	// 	AllowHeaders: []string{
-	// 		"Access-Control-Allow-Credentials",
-	// 		"Access-Control-Allow-Headers",
-	// 		"Access-Control-Allow-Origin",
-	// 		"Content-Type",
-	// 		"Content-Length",
-	// 		"Accept-Encoding",
-	// 		"Authorization",
-	// 	},
-	// 	// cookieなどの情報を必要とするかどうか
-	// 	AllowCredentials: true,
-	// 	// preflightリクエストの結果をキャッシュする時間
-	// 	MaxAge: 24 * time.Hour,
-	// }))
 
 	api := r.Group("/api")
 	{

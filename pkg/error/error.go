e8b2432897e145a8088a5c3b60e1bbb5e612bb00
nas <vividnasubi@gmail.com>
2023-12-27 01:49:22 +0900

    feat: メールアドレスとユーザー名のカスタムエラーの実装

--- pkg/error/error.go
+++ pkg/error/error.go
@@ -5,18 +5,34 @@ import (
 	"time"
 )
 
-// BadRequestError HTTP Status Code: 400
-type BadRequestError struct {
+// BadEmailError
+type BadEmailError struct {
 	When time.Time
 }
 
-func NewMyError() error {
-	return &BadRequestError{
+func NewBadEmailError() error {
+	return &BadEmailError{
 		When: time.Now(),
 	}
 }
 
 // Error implements error.
-func (e *BadRequestError) Error() string {
-	return fmt.Sprintf("error at %s", e.When)
+func (e *BadEmailError) Error() string {
+	return fmt.Sprintf("メールアドレスの形式が正しくありません %s", e.When)
+}
+
+// BadNameError
+type BadNameError struct {
+	When time.Time
+}
+
+func NewBadNameError() error {
+	return &BadNameError{
+		When: time.Now(),
+	}
+}
+
+// Error implements error.
+func (e *BadNameError) Error() string {
+	return fmt.Sprintf("ユーザー名の形式が正しくありません %s", e.When)
 }

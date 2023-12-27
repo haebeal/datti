1aad62b40e1b438fbe18c092719af82b9297bc40
nas <vividnasubi@gmail.com>
2023-12-27 20:01:36 +0900

    fix: userCreateハンドラの仕様変更

--- pkg/interface/api/handler/user.go
+++ pkg/interface/api/handler/user.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/datti-api/pkg/domain/model"
 	"github.com/datti-api/pkg/usecase"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type UserHandler interface {
@@ -27,15 +29,20 @@ func NewUserHandler(userUseCase usecase.UserUseCase) UserHandler {
 // HandlerCreate implements UserHandler.
 func (uh *userHandler) HandlerCreate(c *gin.Context) {
 	user := new(model.User)
-	name, exsist := c.Get("name")
-	if exsist {
-		user.Name = name.(string)
-	}
-	email, exsist := c.Get("email")
-	if exsist {
-		user.Email = email.(string)
+	// name, exsist := c.Get("name")
+	// if exsist {
+	// 	user.Name = name.(string)
+	// }
+	// email, exsist := c.Get("email")
+	// if exsist {
+	// 	user.Email = email.(string)
+	// }
+	// リクエストボディから構造体へバインディング
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 	}
 
+	// ユーザー情報の新規登録
 	newUser, err := uh.useCase.CreateUser(c, user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
@@ -58,7 +65,11 @@ func (uh *userHandler) HandlerGet(c *gin.Context) {
 
 	findUser, err := uh.useCase.GetUserByEmail(c, user)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"err": err.Error()})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+		}
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": findUser})
 	}

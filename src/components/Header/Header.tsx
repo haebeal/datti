3e2720e050b1e9d2b4e1b083dbf8dee8bd1275ad
rom6621 <3103rom@gmail.com>
2024-01-05 23:29:21 +0900

    feat: profile情報をsessionから取得するように変更

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,18 @@
 import { Box, Container } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
-import useSWR from "swr";
-
-import { getProfile } from "@/features/profile";
 
 import { HeaderContents } from "./HeaderContents";
 
 export const Header = () => {
   const { data: session, status } = useSession();
 
-  const { isLoading, data } = useSWR(
-    session?.credential.accessToken ?? null,
-    (accessToken) => getProfile(accessToken),
-  );
-
   return (
     <Box as="header" h="80px" bg="white">
       <Container maxW="container.xl" h="full">
         <HeaderContents
-          isLoading={isLoading || status === "loading"}
-          name={data?.name}
-          photoUrl={data?.photoUrl}
+          isLoading={status === "loading"}
+          name={session?.profile?.name}
+          photoUrl={session?.profile?.photoUrl}
         />
       </Container>
     </Box>

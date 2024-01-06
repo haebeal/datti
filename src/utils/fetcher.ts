29ce86585efd0f998ee020832448260b351cc3d5
rom6621 <3103rom@gmail.com>
2024-01-06 20:21:11 +0900

    fix: bodyを使用しないように変更

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -4,14 +4,12 @@ export const fetcher = async <T extends object>(
   path: string,
   accessToken: string | null | undefined,
   method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
-  body?: Partial<T>,
 ): Promise<T> => {
   const response = await fetch(path, {
     method: method,
     headers: {
       Authorization: `Bearer: ${accessToken}`,
     },
-    body: body ? JSON.stringify(body) : undefined,
   });
   const result = await response.json();
 

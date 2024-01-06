a1afa9b55a6dc01c4bfe3d345d42cdea7a0f7f1e
nas <vividnasubi@gmail.com>
2024-01-06 23:14:56 +0900

    feat: ユーザー更新機能のユースケース層の実装

--- pkg/usecase/user.go
+++ pkg/usecase/user.go
@@ -12,7 +12,7 @@ type UserUseCase interface {
 	CreateUser(c context.Context, user *model.User) (*model.User, error)
 	GetUser(c context.Context, id int) (*model.User, error)
 	GetUserByEmail(c context.Context, user *model.User) (*model.User, error)
-	UpdateUser(c context.Context, user *model.User)
+	UpdateUser(c context.Context, user *model.User) (*model.User, error)
 }
 
 type userUseCase struct {
@@ -56,6 +56,16 @@ func (uu *userUseCase) GetUserByEmail(c context.Context, user *model.User) (*mod
 }
 
 // UpdateUser implements UserUseCase.
-func (uu *userUseCase) UpdateUser(c context.Context, user *model.User) {
-	panic("unimplemented")
+func (uu *userUseCase) UpdateUser(c context.Context, user *model.User) (*model.User, error) {
+	err := validator.ValidatorName(user.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	updateUser, err := uu.repository.UpdateUser(c, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return updateUser, nil
 }

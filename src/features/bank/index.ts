05db9796765ef23c06f83c3f710866535e04a724
rom6621 <3103rom@gmail.com>
2024-01-08 21:01:32 +0900

    fix: export修正

--- src/features/bank/index.ts
+++ src/features/bank/index.ts
@@ -1,6 +1,6 @@
 export * from "./api/getBanks";
 export * from "./api/getBank";
-export * from "./api/getBranches";
+export * from "./api/getBranch";
 export * from "./api/getBranches";
 
 export type * from "./types";

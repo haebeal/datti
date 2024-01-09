b070a84f8dbfc43512a20a9d372ad44b43d5422d
nas <vividnasubi@gmail.com>
2024-01-10 00:02:32 +0900

    feat: 環境変数の追加

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/datti-api/pkg/interface/api/server"
 	"github.com/joho/godotenv"
@@ -16,5 +17,10 @@ func main() {
 
 	// .envの DSNを取得して、messageに代入します。
 	dsn := os.Getenv("DSN")
-	server.Sever(dsn)
+	hostName := os.Getenv("HOST_NAME")
+	dbInit, err := strconv.ParseBool(os.Getenv("DB_INIT"))
+	if err != nil {
+		panic("failed to lodad .env DB_INIT")
+	}
+	server.Sever(dsn, hostName, dbInit)
 }

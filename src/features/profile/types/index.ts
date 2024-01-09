f1c513f6998450ed2b47ea018636f22dc14440cc
rom6621 <3103rom@gmail.com>
2024-01-09 19:57:34 +0900

    feat: profileのzodオブジェクト化

--- src/features/profile/types/index.ts
+++ src/features/profile/types/index.ts
@@ -1,9 +1,15 @@
-export type Profile = {
-  id: string;
-  name: string;
-  email: string;
-  photoUrl: string;
-  accountCode: string;
-  bankCode: string;
-  branchCode: string;
-};
+import { z } from "zod";
+
+export const profileScheme = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+  photoUrl: z.string().url(),
+  accountCode: z.string().length(7, {
+    message: "口座番号は7桁で入力してください",
+  }),
+  bankCode: z.string().min(3).max(4),
+  branchCode: z.string().length(3),
+});
+
+export type Profile = z.infer<typeof profileScheme>;

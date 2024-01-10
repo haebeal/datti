30889b0f5ab83528c0b3b5086ecd094988a15f21
nas <vividnasubi@gmail.com>
2024-01-11 01:16:50 +0900

    fix: レスポンスJSONの変更

--- pkg/interface/api/handler/user.go
+++ pkg/interface/api/handler/user.go
@@ -59,9 +59,9 @@ func (uh *userHandler) HandlerGet(c *gin.Context) {
 	findUser, err := uh.useCase.GetUserByEmail(c, user)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"Error": err.Error()})
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		} else {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"Error": err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
 	} else {
 		c.JSON(http.StatusOK, findUser)

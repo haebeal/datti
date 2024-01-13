1fa91c86f52b2bbf198b241a1dde80d96151dbb8
nas <vividnasubi@gmail.com>
2024-01-13 17:43:45 +0900

    feat: インターフェースGroupRepositoryの定義

---
+++ pkg/domain/repository/group.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/datti-api/pkg/domain/model"
+)
+
+type GroupRepository interface {
+	Creat(c context.Context, group *model.Group, owner *model.Group, members []*model.User) (*model.Group, []*model.User, error)
+	Get(c context.Context, user *model.User) (*model.Group, []*model.User, error)
+	Update(c context.Context, members []*model.User, gropu *model.Group) (*model.Group, []*model.User, error)
+	//Delete()
+}

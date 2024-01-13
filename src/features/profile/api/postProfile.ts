73d1e07cccc233e4280ed5f051781638368fac48
rom6621 <3103rom@gmail.com>
2024-01-13 21:39:51 +0900

    fix: 環境変数名を修正

--- src/features/profile/api/postProfile.ts
+++ src/features/profile/api/postProfile.ts
@@ -7,7 +7,7 @@ export const postProfile = (
   body: Partial<Profile>,
 ): Promise<Profile> => {
   return fetcher<Profile>(
-    `${process.env.NEXT_PUBLIC_FETCH_HOST}/api/me`,
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/me`,
     accessToken,
     "PUT",
     body,

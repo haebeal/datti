ebae665df014af357497354ca0bf8208c3c4ec83
rom6621 <3103rom@gmail.com>
2024-01-13 21:19:49 +0900

    feat: profileのhooksをauth0を使用するように変更

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,32 +1,52 @@
 import { useToast } from "@chakra-ui/react";
-import useSWR from "swr";
 
-import { Profile, getProfile, updateProfile } from "@/features/profile";
-import { useSession } from "next-auth/react";
+import { Profile, postProfile, profileScheme } from "@/features/profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const useProfile = () => {
-  const { data: session, status } = useSession();
+  const { user, isLoading, getAccessTokenSilently, getAccessTokenWithPopup } =
+    useAuth0();
   const toast = useToast();
 
-  const {
-    data: profile,
-    isLoading: isFetching,
-    mutate,
-  } = useSWR<Profile>(session?.credential.accessToken, getProfile);
+  const getAccessToken = process.env.NEXT_PUBLIC_BASE_URL.startsWith(
+    "http://localhost",
+  )
+    ? getAccessTokenWithPopup
+    : getAccessTokenSilently;
 
-  const update = async (value: Partial<Profile>) => {
-    if (!profile || !session?.credential.accessToken) {
-      return;
+  const profile = profileScheme.parse(user);
+
+  const updateProfile = async (value: Partial<Profile>) => {
+    const accessToken = await getAccessToken();
+    if (!accessToken) {
+      toast({
+        status: "error",
+        title: "アクセストークンの取得に失敗しました",
+      });
+      return null;
     }
-    await updateProfile(session.credential.accessToken, value);
-    toast({
-      title: "プロフィールを更新しました",
-      status: "success",
-    });
-    mutate();
+
+    try {
+      const result = await postProfile(accessToken, value);
+      toast({
+        status: "success",
+        title: "プロフィールを更新しました",
+      });
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast({
+          status: "error",
+          title: error.message,
+        });
+      } else {
+        toast({
+          status: "error",
+          title: "不明なエラーが発生しました",
+        });
+      }
+    }
   };
 
-  const isLoading = status === "loading" || isFetching;
-
-  return { profile, isLoading, update };
+  return { profile, isLoading, updateProfile };
 };

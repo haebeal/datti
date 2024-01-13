cb2aab644872b07a46da11fe4bdb8643ce5273bf
rom6621 <3103rom@gmail.com>
2024-01-13 21:41:54 +0900

    fix: userがundefinedの際にパースしないよう修正

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -14,7 +14,7 @@ export const useProfile = () => {
     ? getAccessTokenWithPopup
     : getAccessTokenSilently;
 
-  const profile = isLoading && !user ? undefined : profileScheme.parse(user);
+  const profile = user === undefined ? undefined : profileScheme.parse(user);
 
   const updateProfile = async (value: Partial<Profile>) => {
     const accessToken = await getAccessToken();

4d5a357dbd6b4a8c30eec6549a5b1c0d293f8345
nas <vividnasubi@gmail.com>
2024-01-16 22:38:32 +0900

    feat: bankAccountスキーマの実装

--- pkg/domain/model/user.go
+++ pkg/domain/model/user.go
@@ -4,13 +4,14 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name        string      `json:"name" gorm:"not null"`
-	Email       string      `json:"email" gorm:"not null;unique"`
-	PhotoUrl    string      `json:"photoUrl"`
-	AccountCode string      `json:"accountCode"`
-	BankCode    string      `json:"bankCode" gorm:"default:null"`
-	BranchCode  string      `json:"branchCode"`
-	GroupUser   []GroupUser `gorm:"foreignKey:UserID"`
+	Name        string        `json:"name" gorm:"not null"`
+	Email       string        `json:"email" gorm:"not null;unique"`
+	PhotoUrl    string        `json:"photoUrl"`
+	AccountCode string        `json:"accountCode"`
+	BankCode    string        `json:"bankCode" gorm:"default:null"`
+	BranchCode  string        `json:"branchCode"`
+	GroupUser   []GroupUser   `gorm:"foreignKey:UserID"`
+	BankAccount []BankAccount `gorm:"foreignKey:UserID"`
 	// PaymentsPaidBy []Payment   `gorm:"foreignKey:PaidBy"`
 	// PaymentsPaidTo []Payment   `gorm:"foreignKey:PaidTo"`
 }

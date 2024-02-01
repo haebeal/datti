23c97f07056b0cd87241038d790f2ee1e17e9c5d
rom6621 <3103rom@gmail.com>
2024-02-01 21:59:28 +0900

    feat: firebase storageにプロフィール画像を挙げられるように実装

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,11 +1,12 @@
 import { Profile } from "@/features/profile";
-import { auth } from "@/utils/firebase";
+import { auth, storage } from "@/utils/firebase";
 import { useToast } from "@chakra-ui/react";
 import {
   GoogleAuthProvider,
   signInWithCredential,
   updateProfile as firebaseUpdateProfile,
 } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
@@ -13,6 +14,7 @@ export const useProfile = () => {
   const { data: session } = useSession();
   const toast = useToast();
   const [isLoading, setLoading] = useState(false);
+  const [isUploading, setUploading] = useState(false);
   const [idToken, setIdToken] = useState<string | null>("");
   const [profile, setProfile] = useState<Profile | null>();
 
@@ -23,7 +25,7 @@ export const useProfile = () => {
       setIdToken(null);
       setProfile(null);
     }
-  }, [session]);
+  }, []);
 
   const signInFirebaseAuth = async (googleIdToken: string) => {
     setLoading(true);
@@ -38,21 +40,45 @@ export const useProfile = () => {
     setLoading(false);
   };
 
-  const updateProfile = async (data: Profile) => {
-    if (auth.currentUser) {
+  const updateProfile = async (data: Partial<Profile>) => {
+    if (auth.currentUser && profile) {
       setLoading(true);
       await firebaseUpdateProfile(auth.currentUser, {
         displayName: data.name,
-        photoURL: data.picture,
       });
       toast({
         title: "プロフィールを更新しました",
         status: "success",
       });
-      setProfile(data);
+      setProfile({ ...profile, name: data.name ?? profile.name });
       setLoading(false);
     }
   };
 
-  return { isLoading, profile, idToken, updateProfile };
+  const uploadProfilePhoto = async (file: File) => {
+    if (auth.currentUser && profile) {
+      setUploading(true);
+      const storageRef = ref(storage, `images/profile/${auth.currentUser.uid}`);
+      await uploadBytes(storageRef, file);
+      const photoURL = await getDownloadURL(storageRef);
+      toast({
+        title: "プロフィール画像を更新しました",
+        status: "success",
+      });
+      await firebaseUpdateProfile(auth.currentUser, {
+        photoURL,
+      });
+      setProfile({ ...profile, picture: photoURL });
+      setUploading(false);
+    }
+  };
+
+  return {
+    isLoading,
+    isUploading,
+    profile,
+    idToken,
+    updateProfile,
+    uploadProfilePhoto,
+  };
 };
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 import { FirebaseOptions, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,4 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
+export const storage = getStorage(app);
 export const auth = getAuth(app);

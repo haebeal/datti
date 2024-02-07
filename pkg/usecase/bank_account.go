9656cb227bd245e06a845370c62aad6df1c5a15d
nas <vividnasubi@gmail.com>
2024-02-07 22:29:00 +0900

    feat: 金融機関情報のバリデーターを実装

--- pkg/usecase/bank_account.go
+++ pkg/usecase/bank_account.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/datti-api/pkg/domain/model"
 	"github.com/datti-api/pkg/domain/repository"
+	"github.com/datti-api/pkg/validator"
 )
 
 type BankAccountUseCase interface {
@@ -19,6 +20,15 @@ type bankAccountUseCase struct {
 
 // CreateBankAccount implements BankAccountUseCase.
 func (bu *bankAccountUseCase) UpsertBankAccount(c context.Context, bank *model.BankAccount) (*model.BankAccount, error) {
+	if err := validator.ValidatorAccountCode(bank.AccountCode); err != nil {
+		return nil, err
+	}
+	if err := validator.ValidatorBankCode(bank.BankCode); err != nil {
+		return nil, err
+	}
+	if err := validator.ValidatorBranchCode(bank.BranchCode); err != nil {
+		return nil, err
+	}
 	newBankAccount, err := bu.repository.UpsertBankAccount(c, bank)
 	if err != nil {
 		return nil, err
--- pkg/validator/bank_account.go
+++ pkg/validator/bank_account.go
@@ -2,32 +2,7 @@ package validator
 
 import (
 	validation "github.com/go-ozzo/ozzo-validation"
-	"github.com/go-ozzo/ozzo-validation/is"
 )
-
-// メールアドレスの検査
-func ValidatorEmail(email string) error {
-	return validation.Validate(email,
-		validation.Required.Error("メールアドレスは必須項目です"),
-		validation.Length(1, 255).Error("メールアドレスは1~255文字で登録してください"),
-		is.Email.Error("メールアドレスを入力してください"),
-	)
-}
-
-// 画像URLの検査
-func ValidatorPhotoUrl(photoUrl string) error {
-	return validation.Validate(photoUrl,
-		is.URL.Error("正しいURLを登録してください"),
-	)
-}
-
-// ユーザー名の検査
-func ValidatorName(name string) error {
-	return validation.Validate(name,
-		validation.Required.Error("ユーザー名は必須項目です"),
-		validation.Length(1, 255).Error("ユーザー名は1~255文字で登録してください"),
-	)
-}
 
 // 金融機関番号の検査
 func ValidatorBankCode(bankCode string) error {

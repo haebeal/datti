fb551675e55d82f67a91dd1eae578445d328e7d0
rom6621 <3103rom@gmail.com>
2024-02-10 11:50:24 +0900

    feat: APIクライアントを作成

---
+++ src/api/bank/index.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+import type * as Types from '../@types'
+
+export type Methods = {
+  /** 口座情報情報の登録 */
+  post: {
+    status: 201
+    /** 正常処理のレスポンス */
+    resBody: Types.BankAccount
+    reqBody: Types.RequestBankAccount
+  }
+
+  /** 口座情報情報の参照 */
+  get: {
+    status: 200
+    /** 正常処理のレスポンス */
+    resBody: Types.BankAccount
+  }
+
+  /** 口座情報情報の削除 */
+  delete: {
+    status: 200
+
+    /** 正常処理のレスポンス */
+    resBody: {
+      message?: string | undefined
+    }
+  }
+}

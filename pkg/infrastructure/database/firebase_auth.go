3d090fa33349eb73db077ebec2c30a7d6d406832
nas <vividnasubi@gmail.com>
2024-02-13 00:18:05 +0900

    feat: firebase authenticationのリポジトリ層の実装

---
+++ pkg/infrastructure/database/firebase_auth.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+	"github.com/datti-api/pkg/interface/response"
+	"google.golang.org/api/option"
+)
+
+type FireBaseTenantClient struct {
+	tenantClient *auth.TenantClient
+}
+
+func NewFireBaseClient() (*FireBaseTenantClient, error) {
+	c := context.Background()
+	errRes := new(response.Error)
+	// Firebase SDKの初期化
+	credential := []byte(os.Getenv("GOOGLE_CREDENTIALS_JSON"))
+	opt := option.WithCredentialsJSON(credential)
+	app, err := firebase.NewApp(c, nil, opt)
+	if err != nil {
+		log.Printf("Error initializing Firebase app: %v", err)
+		errRes.Error = err.Error()
+		return nil, err
+	}
+
+	// authClientの初期化
+	client, err := app.Auth(c)
+	if err != nil {
+		log.Printf("failed init auht client %v/n", err)
+		errRes.Error = err.Error()
+		return nil, err
+	}
+
+	tenantClient, err := client.TenantManager.AuthForTenant(os.Getenv("FIREBASE_AUTH_TENANT"))
+	if err != nil {
+		log.Printf("failed init auht client %v/n", err)
+		errRes.Error = err.Error()
+		return nil, err
+	}
+
+	return &FireBaseTenantClient{
+		tenantClient: tenantClient,
+	}, nil
+}

c32c58f06c91fd2a03285eb17cbc20b646c6c391
rom6621 <3103rom@gmail.com>
2024-02-17 18:55:12 +0900

    feat: AvatorMenuコンポーネントの削除

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,35 @@
 import {
+  Avatar,
   Box,
   Button,
   Container,
   HStack,
   Heading,
   Link,
+  Menu,
+  MenuButton,
+  MenuGroup,
+  MenuItem,
+  MenuList,
   Spacer,
   useMediaQuery,
 } from "@chakra-ui/react";
+import { signOut, useSession } from "next-auth/react";
 
-import { useFirebase } from "@/hooks";
-
-import { AvatarMenu } from "@/components/AvatarMenu";
+import { useProfile } from "@/hooks/useProfile";
 
 export const Header = () => {
   const [isMobile] = useMediaQuery("(max-width: 48em)");
-  const { isLoading, currentUser, signOut } = useFirebase();
+
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated") return;
+
+  const { profile } = useProfile(session.idToken);
+
+  const onClickSignOut = () => {
+    signOut();
+  };
 
   return (
     <Box as="header" h="80px" bg="white">
@@ -25,22 +39,25 @@ export const Header = () => {
             Datti
           </Heading>
           <Spacer />
-          {currentUser !== null && (
-            <>
-              {currentUser ? (
-                <AvatarMenu
-                  isLoading={isLoading}
-                  isMobile={isMobile}
-                  user={currentUser}
-                  signOut={signOut}
-                />
-              ) : null}
-              {!isMobile && (
-                <Button colorScheme="red" onClick={signOut}>
-                  ログアウト
-                </Button>
-              )}
-            </>
+          <Menu>
+            <MenuButton>
+              <Avatar borderColor="gray.100" src={profile?.photoUrl} />
+            </MenuButton>
+            <MenuList>
+              <MenuGroup title={profile?.name}>
+                <MenuItem as={Link} href="/setting">
+                  設定
+                </MenuItem>
+                {isMobile ? (
+                  <MenuItem onClick={onClickSignOut}>ログアウト</MenuItem>
+                ) : null}
+              </MenuGroup>
+            </MenuList>
+          </Menu>
+          {!isMobile && (
+            <Button colorScheme="red" onClick={onClickSignOut}>
+              ログアウト
+            </Button>
           )}
         </HStack>
       </Container>

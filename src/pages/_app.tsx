a2ae31252b7cea98a11879f94dcbd682004149a8
rom6621 <3103rom@gmail.com>
2024-02-17 16:51:33 +0900

    fix: swrを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import { ChakraProvider, useToast } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
-import { SWRConfig } from "swr";
 
 import type { AppPropsWithLayout } from "next/app";
 import type { Session } from "next-auth";
 
 import { theme } from "@/utils";
-
-import { HttpError } from "@/errors";
 
 const App = ({
   Component,
@@ -15,26 +12,9 @@ const App = ({
 }: AppPropsWithLayout<{ session: Session }>) => {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  const toast = useToast();
-
   return (
     <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <SWRConfig
-          value={{
-            onError: (error) => {
-              if (error instanceof HttpError) {
-                toast({
-                  status: "error",
-                  title: error.message,
-                });
-              }
-            },
-          }}
-        >
-          {getLayout(<Component />)}
-        </SWRConfig>
-      </ChakraProvider>
+      <ChakraProvider theme={theme}>{getLayout(<Component />)}</ChakraProvider>
     </SessionProvider>
   );
 };

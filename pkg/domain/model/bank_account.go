13cd7484ddc5edcf2465a636ee4f64c88e2f1986
nas <vividnasubi@gmail.com>
2024-02-24 05:05:23 +0900

    fix: modelからgormライブラリの実装部分を削除

--- pkg/domain/model/bank_account.go
+++ pkg/domain/model/bank_account.go
@@ -1,17 +1,8 @@
 package model
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type BankAccount struct {
-	UserID      string         `json:"uid" gorm:"primarykey"`
-	CreatedAt   time.Time      `json:"createdAt"`
-	UpdatedAt   time.Time      `json:"updatedAt"`
-	DeletedAt   gorm.DeletedAt `json:"deletedAt" gorm:"index"`
-	AccountCode string         `json:"accountCode"`
-	BankCode    string         `json:"bankCode"`
-	BranchCode  string         `json:"branchCode"`
+	UserID      string `json:"uid" gorm:"primarykey"`
+	AccountCode string `json:"accountCode"`
+	BankCode    string `json:"bankCode"`
+	BranchCode  string `json:"branchCode"`
 }
--- pkg/domain/model/group_user.go
+++ pkg/domain/model/group_user.go
@@ -1,18 +1,7 @@
 package model
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type GroupUser struct {
-	UserID    string         `json:"uid" gorm:"primarykey"`
-	GroupID   string         `json:"groupId" gorm:"primarykey"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
-	Owner     bool           `gorm:"not null"`
-	// EventsUserID  []Event `gorm:"foreignKey:CreatedBy;references:UserID"` // CreatedBy列の外部キー
-	// EventsGroupID []Event `gorm:"foreignKey:GroupID;references:GroupID"`  // GroupID列の外部キー
+	UserID  string `json:"uid"`
+	GroupID string `json:"groupId"`
+	Owner   bool   `gorm:"not null"`
 }

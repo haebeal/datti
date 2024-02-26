a2e7c741842512a7e699dbb4ec350c5626f13c67
rom6621 <3103rom@gmail.com>
2024-02-27 00:23:20 +0900

    fix: 表示崩れ対策として改行を行うように修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
-import { Button, Grid, GridItem, Heading, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Grid,
+  GridItem,
+  Heading,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -14,8 +21,11 @@ const Home = () => (
       <Grid templateColumns="repeat(12, 1fr)" minH="100vh" bg="blue.400">
         <GridItem colSpan={12}>
           <VStack direction="column" pt={180} gap={8} color="white">
-            <Heading size="4xl">Hello Datti!</Heading>
-            <Heading size="lg">誰にいくら払ったっけ？を記録するアプリ</Heading>
+            <Heading size="4xl">Datti</Heading>
+            <Stack align="center">
+              <Heading size="lg">誰にいくら払ったっけ？</Heading>
+              <Heading size="lg">を記録するアプリ</Heading>
+            </Stack>
             <Button
               colorScheme="facebook"
               size="lg"

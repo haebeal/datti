68ce81dd253b74389de6a1fd916df4c178a62986
nas <vividnasubi@gmail.com>
2024-03-02 04:45:54 +0900

    feat: bankAccountモデルを実装

--- pkg/domain/model/bank_account.go
+++ pkg/domain/model/bank_account.go
@@ -1,8 +1,15 @@
 package model
 
+import (
+	"time"
+)
+
 type BankAccount struct {
-	UserID      string `json:"uid"`
-	AccountCode string `json:"accountCode"`
-	BankCode    string `json:"bankCode"`
-	BranchCode  string `json:"branchCode"`
+	UID         string    `bun:"uid,pk"`
+	AccountCode string    `bun:"account_code"`
+	BankCode    string    `bun:"bank_code"`
+	BranchCode  string    `bun:"branch_code"`
+	CreatedAt   time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt   time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
+	DeletedAt   time.Time `bun:"deleted_at,soft_delete"`
 }

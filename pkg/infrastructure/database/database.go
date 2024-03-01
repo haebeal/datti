e7fe7c937440e1581c4e209f6912a3db87044f78
nas <vividnasubi@gmail.com>
2024-03-02 04:47:14 +0900

    feat: bankAccountスキーマからのテーブルマイグレーションを実装

--- pkg/infrastructure/database/database.go
+++ pkg/infrastructure/database/database.go
@@ -1,10 +1,14 @@
 package database
 
 import (
+	"context"
+
+	"github.com/datti-api/pkg/domain/model"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 type DBClient struct {
@@ -14,12 +18,22 @@ type DBClient struct {
 func NewBunClient(dsn string) (*DBClient, error) {
 	config, err := pgx.ParseConfig(dsn)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	config.PreferSimpleProtocol = true
 
 	sqlDB := stdlib.OpenDB(*config)
 	db := bun.NewDB(sqlDB, pgdialect.New())
+
+	// クエリーフックを追加
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+	))
+
+	// テーブルを削除・作成
+	if err := db.ResetModel(context.TODO(), (*model.BankAccount)(nil)); err != nil {
+		return nil, err
+	}
 
 	return &DBClient{Client: db}, nil
 }

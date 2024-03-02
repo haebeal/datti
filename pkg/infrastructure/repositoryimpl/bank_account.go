c61154d00dfab6b7b9bbcec43289667d0d023ebe
nas <vividnasubi@gmail.com>
2024-03-02 18:56:47 +0900

    fix: 口座情報の登録・更新・削除の機能を修正

--- pkg/infrastructure/repositoryimpl/bank_account.go
+++ pkg/infrastructure/repositoryimpl/bank_account.go
@@ -13,18 +13,47 @@ type bankAccountRepositoryImpl struct {
 }
 
 // DeleteBankAccount implements repository.BankAccountRepository.
-func (b *bankAccountRepositoryImpl) DeleteBankAccount(c context.Context, uid string) (*model.BankAccount, error) {
-	panic("unimplemented")
+func (br *bankAccountRepositoryImpl) DeleteBankAccount(c context.Context, uid string) (*model.BankAccount, error) {
+	bankAccount := new(model.BankAccount)
+	_, err := br.DBEngine.Client.NewDelete().Model(bankAccount).Where("uid = ?", uid).Exec(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return bankAccount, nil
 }
 
 // GetBankAccountById implements repository.BankAccountRepository.
-func (b *bankAccountRepositoryImpl) GetBankAccountById(c context.Context, uid string) (*model.BankAccount, error) {
-	panic("unimplemented")
+func (br *bankAccountRepositoryImpl) GetBankAccountByUid(c context.Context, uid string) (*model.BankAccount, error) {
+	bankAccount := new(model.BankAccount)
+	err := br.DBEngine.Client.NewSelect().Model(bankAccount).Where("uid = ?", uid).Scan(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return bankAccount, nil
 }
 
 // UpsertBankAccount implements repository.BankAccountRepository.
-func (b *bankAccountRepositoryImpl) UpsertBankAccount(c context.Context, uid string, accountCode string, bankCode string, branchCode string) (string, error) {
-	panic("unimplemented")
+func (br *bankAccountRepositoryImpl) UpsertBankAccount(c context.Context, uid string, accountCode string, bankCode string, branchCode string) (*model.BankAccount, error) {
+	bankAccount := new(model.BankAccount)
+	err := br.DBEngine.Client.NewInsert().
+		Model(bankAccount).
+		Value("uid", "?", uid).
+		Value("account_code", "?", accountCode).
+		Value("bank_code", "?", bankCode).
+		Value("branch_code", "?", branchCode).
+		On("CONFLICT (uid) DO UPDATE").
+		Set("account_code = ?", accountCode).
+		Set("bank_code = ?", bankCode).
+		Set("branch_code = ?", branchCode).
+		Set("deleted_at = ?", nil).
+		Scan(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return bankAccount, nil
 }
 
 func NewBankAccountRepository(engine *database.DBClient) repository.BankAccountRepository {

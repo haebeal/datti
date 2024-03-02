5e72d0866736c24904f1f362c30c316b1fb183c3
nas <vividnasubi@gmail.com>
2024-03-02 18:54:03 +0900

    feat: bankAccountスキーマの修正

--- pkg/domain/model/bank_account.go
+++ pkg/domain/model/bank_account.go
@@ -5,11 +5,11 @@ import (
 )
 
 type BankAccount struct {
-	UID         string    `bun:"uid,pk"`
-	AccountCode string    `bun:"account_code"`
-	BankCode    string    `bun:"bank_code"`
-	BranchCode  string    `bun:"branch_code"`
-	CreatedAt   time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
-	UpdatedAt   time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
-	DeletedAt   time.Time `bun:"deleted_at,soft_delete"`
+	UID         string     `bun:"uid,pk"`
+	AccountCode string     `bun:"account_code"`
+	BankCode    string     `bun:"bank_code"`
+	BranchCode  string     `bun:"branch_code"`
+	CreatedAt   time.Time  `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt   time.Time  `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
+	DeletedAt   *time.Time `bun:"deleted_at,soft_delete"`
 }

834a082c73c2a1da3697c7f91e7f95d85a034726
nas <vividnasubi@gmail.com>
2024-03-15 23:49:23 +0900

    fix: groupUserスキーマのGroupIDフィールの構造体タグをgroup_idに変更

--- pkg/domain/model/group_user.go
+++ pkg/domain/model/group_user.go
@@ -2,6 +2,6 @@ package model
 
 type GroupUser struct {
 	UserID  string `bun:"uid,pk"`
-	GroupID string `bun:"groupId,pk"`
+	GroupID string `bun:"group_id,pk"`
 	Owner   bool   `bun:"owner,notnull"`
 }

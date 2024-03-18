3e30a674dd54b404a7cbf5dda7708ab097d0b723
nas <vividnasubi@gmail.com>
2024-03-19 00:24:25 +0900

    feat: groupスキーマに対するリクエストハンドラの実装

--- pkg/interface/api/handler/group.go
+++ pkg/interface/api/handler/group.go
@@ -1,6 +1,10 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/datti-api/pkg/domain/model"
+	"github.com/datti-api/pkg/interface/response"
 	"github.com/datti-api/pkg/usecase"
 	"github.com/labstack/echo/v4"
 )
@@ -24,22 +28,88 @@ func (g *groupHandler) HandleCreate(c echo.Context) error {
 
 // HandleGet implements GroupHandler.
 func (g *groupHandler) HandleGet(c echo.Context) error {
-	panic("unimplemented")
+	errResponse := new(response.Error)
+	uid := c.Get("uid").(string)
+
+	groups, err := g.useCase.GetGroups(c.Request().Context(), uid)
+	if err != nil {
+		errResponse.Error = err.Error()
+		return c.JSON(http.StatusInternalServerError, errResponse)
+	} else {
+		return c.JSON(http.StatusOK, groups)
+	}
 }
 
 // HandleGetById implements GroupHandler.
 func (g *groupHandler) HandleGetById(c echo.Context) error {
-	panic("unimplemented")
+	errResponse := new(response.Error)
+	id := c.Param("id")
+
+	group, members, err := g.useCase.GetGroupById(c.Request().Context(), id)
+	if err != nil {
+		errResponse.Error = err.Error()
+		return c.JSON(http.StatusInternalServerError, errResponse)
+	} else {
+		return c.JSON(http.StatusOK, struct {
+			ID    string        `json:"id"`
+			Name  string        `json:"name"`
+			Users []*model.User `json:"users"`
+		}{
+			ID:    group.ID,
+			Name:  group.Name,
+			Users: members,
+		})
+	}
 }
 
 // HandleRegisterd implements GroupHandler.
 func (g *groupHandler) HandleRegisterd(c echo.Context) error {
-	panic("unimplemented")
+	uids := make([]string, 0)
+	errResponse := new(response.Error)
+	id := c.Param("id")
+
+	group, members, err := g.useCase.RegisterdMembers(c.Request().Context(), id, uids)
+	if err != nil {
+		errResponse.Error = err.Error()
+		return c.JSON(http.StatusInternalServerError, errResponse)
+	} else {
+		return c.JSON(http.StatusOK, struct {
+			ID    string        `json:"id"`
+			Name  string        `json:"name"`
+			Users []*model.User `json:"users"`
+		}{
+			ID:    group.ID,
+			Name:  group.Name,
+			Users: members,
+		})
+	}
 }
 
 // HandleUpdate implements GroupHandler.
 func (g *groupHandler) HandleUpdate(c echo.Context) error {
-	panic("unimplemented")
+	var name string
+	errResponse := new(response.Error)
+	id := c.Param("id")
+	if err := c.Bind(&name); err != nil {
+		errResponse.Error = err.Error()
+		return c.JSON(http.StatusInternalServerError, errResponse)
+	}
+
+	group, members, err := g.useCase.UpdateGroup(c.Request().Context(), id, name)
+	if err != nil {
+		errResponse.Error = err.Error()
+		return c.JSON(http.StatusInternalServerError, errResponse)
+	} else {
+		return c.JSON(http.StatusOK, struct {
+			ID    string        `json:"id"`
+			Name  string        `json:"name"`
+			Users []*model.User `json:"users"`
+		}{
+			ID:    group.ID,
+			Name:  group.Name,
+			Users: members,
+		})
+	}
 }
 
 func NewGroupHandler(groupUseCase usecase.GroupUseCase) GroupHandler {

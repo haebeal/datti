74a12c8e74aab3fc83f9878aabb1aa4285645ddd
rom6621 <3103rom@gmail.com>
2024-05-05 15:20:32 +0900

    feat: フレンドステータスによる承認・取り消しボタンを実装

--- app/components/FriendCard/FriendCard.tsx
+++ app/components/FriendCard/FriendCard.tsx
@@ -1,4 +1,4 @@
-import { Form, useNavigation } from "@remix-run/react";
+import { Form, useLocation, useNavigation } from "@remix-run/react";
 import { User } from "~/api/datti/@types";
 import {
   AlertDialog,
@@ -18,7 +18,11 @@ interface Props {
 }
 
 export function FriendCard({ friend }: Props) {
+  const { search } = useLocation();
   const { state } = useNavigation();
+  const searchParams = new URLSearchParams(search);
+
+  const status = searchParams.get("status");
 
   return (
     <div className="flex flex-row  w-full bg-white px-6 py-5 gap-5 items-center rounded-md border border-gray-200">
@@ -28,33 +32,75 @@ export function FriendCard({ friend }: Props) {
       </Avatar>
       <h1 className="text-lg font-bold mr-auto">{friend.name}</h1>
 
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
+      {status === "friends" ? (
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              disabled={state === "submitting"}
+              className="bg-red-500 hover:bg-red-600 font-semibold"
+            >
+              解除
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              {friend.name}をフレンドから解除しますか?
+            </AlertDialogHeader>
+            <AlertDialogDescription>
+              フレンドを解除すると相手からもフレンドではなくなります。
+              <br />
+              本当によろしいですか？
+            </AlertDialogDescription>
+            <AlertDialogFooter>
+              <AlertDialogCancel>キャンセル</AlertDialogCancel>
+              <Form method="delete">
+                <input type="hidden" name="uid" value={friend.uid} />
+                <AlertDialogAction
+                  disabled={state === "submitting"}
+                  className="font-semibold bg-red-500 hover:bg-red-600"
+                  type="submit"
+                >
+                  解除
+                </AlertDialogAction>
+              </Form>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      ) : status === "requests" ? (
+        <Form method="delete">
+          <input type="hidden" name="uid" value={friend.uid} />
           <Button
             disabled={state === "submitting"}
-            className="bg-red-500 hover:bg-red-600 font-semibold"
+            className="font-semibold bg-red-500 hover:bg-red-600"
+            type="submit"
           >
-            解除
+            取り消し
           </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            {friend.name}をフレンドから解除しますか?
-          </AlertDialogHeader>
-          <AlertDialogDescription>
-            フレンドを解除すると相手からもフレンドではなくなります。
-            <br />
-            本当によろしいですか？
-          </AlertDialogDescription>
-          <AlertDialogFooter>
-            <AlertDialogCancel>キャンセル</AlertDialogCancel>
-            <Form method="delete">
-              <input type="hidden" name="uid" value={friend.uid} />
-              <AlertDialogAction type="submit">解除</AlertDialogAction>
-            </Form>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+        </Form>
+      ) : (
+        <>
+          <Form method="post">
+            <input type="hidden" name="uid" value={friend.uid} />
+            <Button
+              disabled={state === "submitting"}
+              className="font-semibold bg-sky-500 hover:bg-sky-600"
+              type="submit"
+            >
+              承認
+            </Button>
+          </Form>
+          <Form method="delete">
+            <input type="hidden" name="uid" value={friend.uid} />
+            <Button
+              disabled={state === "submitting"}
+              className="font-semibold bg-red-500 hover:bg-red-600"
+              type="submit"
+            >
+              却下
+            </Button>
+          </Form>
+        </>
+      )}
     </div>
   );
 }

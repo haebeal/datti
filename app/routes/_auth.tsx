3c9feb29bd38ac7d1744504a959f1138ffa3ead0
rom6621 <3103rom@gmail.com>
2024-05-11 13:10:22 +0900

    feat: ヘッダーコンポーネントの遅延ローディングを実装

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,27 +1,15 @@
-import { Await, Outlet, useLoaderData } from "@remix-run/react";
-import { Suspense } from "react";
-import { AuthLoader } from "~/.server/loaders";
+import { Outlet } from "@remix-run/react";
 import { Header } from "~/components/Header";
 
 export { authLoader as loader } from "~/.server/loaders";
 
 export default function Auth() {
-  const { profile } = useLoaderData<AuthLoader>();
-
   return (
     <div className="min-h-screen">
-      <Suspense fallback={<p>プロフィールのローディング</p>}>
-        <Await resolve={profile}>
-          {(profile) => (
-            <>
-              <Header profile={profile} className="h-20 bg-white" />
-              <div className="container py-3">
-                <Outlet />
-              </div>
-            </>
-          )}
-        </Await>
-      </Suspense>
+      <Header />
+      <div className="container py-3">
+        <Outlet />
+      </div>
     </div>
   );
 }

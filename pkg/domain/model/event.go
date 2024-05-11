b07e14a94b5ee6e6b25e52ac73ac6bb8b748c4ab
nas <vividnasubi@gmail.com>
2024-05-11 20:18:26 +0900

    fix: jsonタグを追加

--- pkg/domain/model/event.go
+++ pkg/domain/model/event.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Event struct {
-	ID        string     `bun:"id"`
-	Name      string     `bun:"name"`
-	CreatedBy string     `bun:"created_by"`
-	GroupId   string     `bun:"group_id"`
-	EventedAt time.Time  `bun:"evented_at, nullzero,notnull"`
-	CreatedAt time.Time  `bun:"created_at,nullzero,notnull,default:current_timestamp"`
-	UpdatedAt time.Time  `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
-	DeletedAt *time.Time `bun:"deleted_at,soft_delete"`
+	ID        string     `bun:"id" json:"id"`
+	Name      string     `bun:"name" json:"name"`
+	CreatedBy string     `bun:"created_by" json:"created_by"`
+	GroupId   string     `bun:"group_id" json:"Group_id"`
+	EventedAt time.Time  `bun:"evented_at, nullzero,notnull" json:"evented_at"`
+	CreatedAt time.Time  `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"created_at"`
+	UpdatedAt time.Time  `bun:"updated_at,nullzero,notnull,default:current_timestamp" json:"updated_at"`
+	DeletedAt *time.Time `bun:"deleted_at,soft_delete" json:"deleted_at"`
 }

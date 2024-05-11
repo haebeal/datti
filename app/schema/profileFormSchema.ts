b7e27ee9c03bb4fe80fadb31dd2f180f58d37905
rom6621 <3103rom@gmail.com>
2024-05-11 18:56:45 +0900

    feat: プロフィールフォームのスキーマを修正

---
+++ app/schema/profileFormSchema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import type { UserUpdateRequest } from "~/api/datti/@types";
+
+import { ToZod } from "~/lib/toZod";
+
+export const profileFormSchema = z.object<ToZod<UserUpdateRequest>>({
+  name: z.string().min(1, {
+    message: "ユーザー名を入力してください",
+  }),
+  photoUrl: z.string().url(),
+  accountCode: z.string().length(7, {
+    message: "口座番号は7桁で入力してください",
+  }),
+  bankCode: z
+    .string()
+    .min(3, {
+      message: "金融機関を選択してください",
+    })
+    .max(4, {
+      message: "金融機関を選択してください",
+    }),
+  branchCode: z.string().length(3, {
+    message: "支店を選択してください",
+  }),
+});

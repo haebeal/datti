07cd7595951c62a5f904f20b8a6aeb20e1b27ea7
rom6621 <3103rom@gmail.com>
2024-05-11 13:00:11 +0900

    feat: プロフィールページの遅延ローディングを実装

--- app/routes/_auth.setting.profile.tsx
+++ app/routes/_auth.setting.profile.tsx
@@ -1,4 +1,5 @@
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { Await, useActionData, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 import { ProfileAction } from "~/.server/actions";
 import { AuthLoader } from "~/.server/loaders";
 import { ProfileForm } from "~/components/ProfileForm";
@@ -6,13 +7,25 @@ import { ProfileForm } from "~/components/ProfileForm";
 export { profileAction as action } from "~/.server/actions";
 export { authLoader as loader } from "~/.server/loaders";
 
+function LoadingSpinner() {
+  return (
+    <div className="w-full min-h-[60vh] grid place-content-center">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent" />
+    </div>
+  );
+}
+
 export default function ProfileSetting() {
   const { profile } = useLoaderData<AuthLoader>();
   const lastResult = useActionData<ProfileAction>();
 
   return (
-    <>
-      <ProfileForm defaultValue={profile} lastResult={lastResult} />
-    </>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Await resolve={profile}>
+        {(profile) => (
+          <ProfileForm defaultValue={profile} lastResult={lastResult} />
+        )}
+      </Await>
+    </Suspense>
   );
 }

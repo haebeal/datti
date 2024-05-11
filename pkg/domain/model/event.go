d87774e46bd589ebf4b5442015250b663161b0ba
nas <vividnasubi@gmail.com>
2024-05-11 20:30:54 +0900

    fix: jsontagのGroup_idをgroup_idに修正

--- pkg/domain/model/event.go
+++ pkg/domain/model/event.go
@@ -6,7 +6,7 @@ type Event struct {
 	ID        string     `bun:"id" json:"id"`
 	Name      string     `bun:"name" json:"name"`
 	CreatedBy string     `bun:"created_by" json:"created_by"`
-	GroupId   string     `bun:"group_id" json:"Group_id"`
+	GroupId   string     `bun:"group_id" json:"group_id"`
 	EventedAt time.Time  `bun:"evented_at, nullzero,notnull" json:"evented_at"`
 	CreatedAt time.Time  `bun:"created_at,nullzero,notnull,default:current_timestamp" json:"created_at"`
 	UpdatedAt time.Time  `bun:"updated_at,nullzero,notnull,default:current_timestamp" json:"updated_at"`

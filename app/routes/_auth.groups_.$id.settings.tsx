fbd7005318f91b87c38c3b2e33cc4a6759a4b354
rom6621 <3103rom@gmail.com>
2024-05-11 15:26:13 +0900

    feat: グループ設定の遅延ローディングの設定

--- app/routes/_auth.groups_.$id.settings.tsx
+++ app/routes/_auth.groups_.$id.settings.tsx
@@ -1,4 +1,5 @@
-import { useActionData, useLoaderData } from "@remix-run/react";
+import { Await, useActionData, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 import { GroupSettingsAction } from "~/.server/actions";
 import { GroupLoader } from "~/.server/loaders";
 import { GroupForm } from "~/components/GroupForm";
@@ -6,17 +7,31 @@ import { GroupForm } from "~/components/GroupForm";
 export { groupSettingsAction as action } from "~/.server/actions";
 export { groupLoader as loader } from "~/.server/loaders";
 
+function LoadingSpinner() {
+  return (
+    <div className="w-full min-h-[60vh] grid place-content-center">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent" />
+    </div>
+  );
+}
+
 export default function GroupSettings() {
   const { group } = useLoaderData<GroupLoader>();
   const lastResult = useActionData<GroupSettingsAction>();
 
   return (
     <div className="py-4">
-      <GroupForm
-        defaultValue={group}
-        lastResult={lastResult}
-        buttonLabel="更新"
-      />
+      <Suspense fallback={<LoadingSpinner />}>
+        <Await resolve={group}>
+          {(group) => (
+            <GroupForm
+              defaultValue={group}
+              lastResult={lastResult}
+              buttonLabel="更新"
+            />
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 }

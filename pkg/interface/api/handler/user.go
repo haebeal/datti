0c68a12176e584fe6509bd249bcd49df3cb2b3b9
nas <vividnasubi@gmail.com>
2024-05-19 02:38:58 +0900

    fix: HandleGetStatusを削除 レスポンスにstatusを追加

--- pkg/interface/api/handler/user.go
+++ pkg/interface/api/handler/user.go
@@ -15,7 +15,6 @@ type UserHandler interface {
 	HandleGetByUid(c echo.Context) error
 	HandleGetByUidWithPahtParam(c echo.Context) error
 	HandleGetByEmail(c echo.Context) error
-	HandleGetStatus(c echo.Context) error
 	HandleUpdate(c echo.Context) error
 }
 
@@ -23,32 +22,14 @@ type userHandler struct {
 	useCase usecase.UserUseCase
 }
 
-// HandleGetStatus implements UserHandler.
-func (u *userHandler) HandleGetStatus(c echo.Context) error {
-	res := new(response.UserStatus)
-	errResponse := new(response.Error)
-	uid := c.Get("uid").(string)
-	fuid := c.Param("userId")
-
-	user, status, err := u.useCase.GetUserStatus(c.Request().Context(), uid, fuid)
-	if err != nil {
-		errResponse.Error = err.Error()
-		return c.JSON(http.StatusInternalServerError, errResponse)
-	} else {
-		res.UID = user.UID
-		res.Name = user.Name
-		res.Status = status
-		return c.JSON(http.StatusOK, res)
-	}
-}
-
 // HandleGetByUidWithPahtParam implements UserHandler.
 func (u *userHandler) HandleGetByUidWithPahtParam(c echo.Context) error {
 	res := new(response.UserWithBankAccount)
 	errResponse := new(response.Error)
-	uid := c.Param("uid")
+	uid := c.Get("uid").(string)
+	targetId := c.Param("uid")
 
-	user, bank, err := u.useCase.GetUserByUid(c.Request().Context(), uid)
+	user, status, bank, err := u.useCase.GetUserByUid(c.Request().Context(), uid, targetId)
 	if err != nil {
 		errResponse.Error = err.Error()
 		return c.JSON(http.StatusInternalServerError, errResponse)
@@ -60,16 +41,18 @@ func (u *userHandler) HandleGetByUidWithPahtParam(c echo.Context) error {
 		res.Bank.BankCode = bank.BankCode
 		res.Bank.BranchCode = bank.BranchCode
 		res.Bank.AccountCode = bank.AccountCode
+		res.Status = status
 		return c.JSON(http.StatusOK, res)
 	}
 }
 
 // HandleGetByEmail implements UserHandler.
 func (u *userHandler) HandleGetByEmail(c echo.Context) error {
+	uid := c.Get("uid").(string)
 	email := c.QueryParam("email")
 	errRes := new(response.Error)
 
-	users, banks, err := u.useCase.GetUsersByEmail(c.Request().Context(), email)
+	users, statuses, banks, err := u.useCase.GetUsersByEmail(c.Request().Context(), uid, email)
 	if err != nil {
 		errRes.Error = err.Error()
 		return c.JSON(http.StatusInternalServerError, errRes)
@@ -83,6 +66,7 @@ func (u *userHandler) HandleGetByEmail(c echo.Context) error {
 			res[i].Bank.AccountCode = banks[i].AccountCode
 			res[i].Bank.BankCode = banks[i].BankCode
 			res[i].Bank.BranchCode = banks[i].BranchCode
+			res[i].Status = statuses[i]
 		}
 		return c.JSON(http.StatusOK, struct {
 			Users []response.UserWithBankAccount `json:"users"`
@@ -98,7 +82,7 @@ func (u *userHandler) HandleGetByUid(c echo.Context) error {
 	errResponse := new(response.Error)
 	uid := c.Get("uid").(string)
 
-	user, bank, err := u.useCase.GetUserByUid(c.Request().Context(), uid)
+	user, status, bank, err := u.useCase.GetUserByUid(c.Request().Context(), uid, uid)
 	if err != nil {
 		errResponse.Error = err.Error()
 		return c.JSON(http.StatusInternalServerError, errResponse)
@@ -110,6 +94,7 @@ func (u *userHandler) HandleGetByUid(c echo.Context) error {
 		res.Bank.BankCode = bank.BankCode
 		res.Bank.BranchCode = bank.BranchCode
 		res.Bank.AccountCode = bank.AccountCode
+		res.Status = status
 		return c.JSON(http.StatusOK, res)
 	}
 }

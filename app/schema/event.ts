34cea1257015e3bdbd841b5c869f347a1d1571bb
rom6621 <3103rom@gmail.com>
2024-06-01 22:52:08 +0900

    feat: イベントフォームのスキーマを支払い入力に対応

--- app/schema/event.ts
+++ app/schema/event.ts
@@ -1,10 +1,24 @@
 import { z } from "zod";
-import { EventCreateRequest, EventUpdateRequest } from "~/api/datti/@types";
+import {
+  EventCreateRequest,
+  EventUpdateRequest,
+  Payment,
+} from "~/api/datti/@types";
 import { ToZod } from "~/lib/toZod";
+
+export const paymentSchema = z.object<ToZod<Payment>>({
+  user: z.string(),
+  amount: z.number(),
+});
 
 export const eventSchema = z.object<
   ToZod<EventCreateRequest | EventUpdateRequest>
 >({
-  name: z.string(),
+  name: z.string({
+    required_error: "名前を入力してください",
+  }),
   evented_at: z.string().datetime(),
+  paid_by: z.string(),
+  amount: z.number(),
+  payments: paymentSchema.array(),
 });

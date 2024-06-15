ffbb00e62dffeb8f0ee89406cd417135aa1213df
rom6621 <3103rom@gmail.com>
2024-06-15 07:00:13 +0000

    fix: イベント作成、イベント更新のスキーマを分離

--- app/schema/eventFormSchema.ts
+++ app/schema/eventFormSchema.ts
@@ -2,9 +2,7 @@ import { z } from "zod";
 import { EventCreateRequest, EventUpdateRequest } from "~/api/@types";
 import { ToZod } from "~/lib/toZod";
 
-export const eventFormSchema = z.object<
-  ToZod<EventCreateRequest | EventUpdateRequest>
->({
+export const eventCreateFormSchema = z.object<ToZod<EventCreateRequest>>({
   name: z.string({
     required_error: "名前を入力してください",
   }),
@@ -13,6 +11,22 @@ export const eventFormSchema = z.object<
   amount: z.number(),
   payments: z.array(
     z.object({
+      paid_to: z.string(),
+      amount: z.number(),
+    })
+  ),
+});
+
+export const eventUpdateFormSchema = z.object<ToZod<EventUpdateRequest>>({
+  name: z.string({
+    required_error: "名前を入力してください",
+  }),
+  evented_at: z.string().datetime(),
+  paid_by: z.string(),
+  amount: z.number(),
+  payments: z.array(
+    z.object({
+      payment_id: z.string(),
       paid_to: z.string(),
       amount: z.number(),
     })

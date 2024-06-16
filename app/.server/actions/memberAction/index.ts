e12db37a2c04a71cbf3b890b4bf8ff393ea3b54a
rom6621 <3103rom@gmail.com>
2024-06-16 03:36:33 +0000

    feat: メンバー操作において、APIからのレスポンスをトースト表示するように実装

--- app/.server/actions/memberAction/index.ts
+++ app/.server/actions/memberAction/index.ts
@@ -1,3 +1,4 @@
+import { HTTPError } from "@aspida/fetch";
 import { ActionFunctionArgs, json } from "@remix-run/cloudflare";
 import { createClient } from "~/lib/apiClient";
 import { getIdToken } from "~/lib/getIdToken.server";
@@ -7,27 +8,51 @@ export const memberAction = async ({
   params,
   context,
 }: ActionFunctionArgs) => {
-  const formData = await request.formData();
-
-  const groupId = params.groupId;
-  const uid = formData.get("uid");
-
-  if (typeof groupId !== "string" || typeof uid !== "string") {
-    throw new Error();
-  }
-
   const { idToken } = await getIdToken({ request, params, context });
   const client = createClient(idToken, context.cloudflare.env.BACKEND_ENDPOINT);
 
+  const formData = await request.formData();
+
+  // メンバー追加処理
   if (request.method === "POST") {
-    await client.groups._groupId(groupId).members.$post({
-      body: {
-        uids: [uid],
-      },
-    });
+    const groupId = params.groupId;
+    const userId = formData.get("uid")?.toString();
+    if (userId === undefined) {
+      return json({
+        message: "ユーザーIDの取得に失敗しました",
+        submission: undefined,
+      });
+    }
+    if (groupId === undefined) {
+      return json({
+        message: "グループIDの取得に失敗しました",
+        submission: undefined,
+      });
+    }
+    try {
+      const { name } = await client.groups._groupId(groupId).members.$post({
+        body: {
+          uids: [userId],
+        },
+      });
+      return json({
+        message: `${name}をメンバーに追加しました`,
+        submission: undefined,
+      });
+    } catch (error) {
+      if (error instanceof HTTPError) {
+        throw new Response(error.message, {
+          status: error.response.status,
+          statusText: error.response.statusText,
+        });
+      }
+    }
   }
 
-  return json({});
+  throw new Response("不明なエラーが発生しました", {
+    status: 500,
+    statusText: "Internal Server Error",
+  });
 };
 
 export type MemberAction = typeof memberAction;

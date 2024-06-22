e1294c759a5a312587729a5994e329a2230fa734
nas <vividnasubi@gmail.com>
2024-06-22 22:27:19 +0900

    feat: イベント削除処理をリポジトリ層に実装

--- pkg/domain/repository/event.go
+++ pkg/domain/repository/event.go
@@ -10,6 +10,7 @@ import (
 type EventRepository interface {
 	CreateEvent(c context.Context, event *model.Event) (*model.Event, error)
 	UpdateEvent(c context.Context, id string, uid string, gid string, name string, eventAt time.Time) (*model.Event, error)
+	DeleteEvent(c context.Context, eventID string) error
 	GetEvent(c context.Context, id string) (*model.Event, error)
 	GetEvents(c context.Context, gid string) ([]*model.Event, error)
 }

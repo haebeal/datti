d5654ab98ae419d4b3426ccb0581df60fcd4e060
rom6621 <3103rom@gmail.com>
2024-06-23 13:27:43 +0000

    feat: 現在の返済一覧ページの実装

--- app/routes/_auth._index.tsx
+++ app/routes/_auth._index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { PaymentList } from "~/components/PaymentList";
+
+export { paymentsLoader as loader } from "~/.server/loaders";
 
 export const meta: MetaFunction = () => [
   { title: "Datti" },
@@ -9,7 +12,10 @@ export default function Index() {
   return (
     <div className="flex flex-col py-3 gap-7">
       <div className="flex items-center justify-between">
-        <h1 className="font-bold text-2xl">現在の支払い</h1>
+        <h1 className="font-bold text-2xl">現在の返済一覧</h1>
+      </div>
+      <div className="rounded-lg bg-white py-3 px-5">
+        <PaymentList />
       </div>
     </div>
   );

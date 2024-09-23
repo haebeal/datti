df21677a3e1d0a0e691df7aae9dfff5ec1dfeb2e
rom6621 <3103rom@gmail.com>
2024-09-23 21:38:53 +0900

    refactor: biomeによるフォーマットを適用

--- app/web/.storybook/main.ts
+++ app/web/.storybook/main.ts
@@ -1,20 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 export default {
-  stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "storybook-addon-remix-react-router",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+	stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		"@storybook/addon-onboarding",
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@chromatic-com/storybook",
+		"@storybook/addon-interactions",
+		"storybook-addon-remix-react-router",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	docs: {
+		autodocs: "tag",
+	},
 } satisfies StorybookConfig;
--- app/web/app/routes/api.auth.signin.tsx
+++ app/web/app/routes/api.auth.signin.tsx
@@ -4,23 +4,23 @@ import { createSupabaseClient } from "~/lib/supabase.server";
 export const loader = () => redirect("/");
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
-  const { headers, supabase } = createSupabaseClient({
-    request,
-    context,
-  });
-  const { origin } = new URL(request.url);
-  const { data } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      queryParams: {
-        access_type: "offline",
-        prompt: "consent",
-      },
-      redirectTo: `${origin}/api/auth/callback/google`,
-    },
-  });
+	const { headers, supabase } = createSupabaseClient({
+		request,
+		context,
+	});
+	const { origin } = new URL(request.url);
+	const { data } = await supabase.auth.signInWithOAuth({
+		provider: "google",
+		options: {
+			queryParams: {
+				access_type: "offline",
+				prompt: "consent",
+			},
+			redirectTo: `${origin}/api/auth/callback/google`,
+		},
+	});
 
-  if (data.url) {
-    return redirect(data.url, { headers }); // use the redirect API for your server framework
-  }
+	if (data.url) {
+		return redirect(data.url, { headers }); // use the redirect API for your server framework
+	}
 };

d534fc95bda7a90d6d019725dfaccf106b18f103
rom6621 <3103rom@gmail.com>
2024-11-04 22:12:51 +0900

    feat: 現在の返済一覧ページのUIを更改

--- app/web/app/features/payments/components/payment-user-card.tsx
+++ app/web/app/features/payments/components/payment-user-card.tsx
@@ -1,5 +1,4 @@
 import type { PaymentUser } from "~/api/@types";
-import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 interface Props {
 	paymentUser: PaymentUser;
@@ -7,13 +6,16 @@ interface Props {
 
 export function PaymentUserCard({ paymentUser: { user, amount } }: Props) {
 	return (
-		<div className="flex flex-row  w-full bg-white px-6 py-5 gap-8 items-center rounded-md border border-gray-200">
-			<Avatar className="border h-14 w-14 border-gray-200">
-				<AvatarImage src={user.photoUrl} />
-				<AvatarFallback>{user.name} photo</AvatarFallback>
-			</Avatar>
-			<h1 className="text-xl font-bold">{user.name}</h1>
-			<h1 className="text-xl font-bold ml-auto mr-32">￥{amount}</h1>
+		<div className="flex flex-row gap-5 items-center">
+			<img
+				src={user.photoUrl}
+				aria-label={`${user.name} photo`}
+				className="rounded-full h-16 w-16"
+			/>
+			<p className="flex md:flex-row flex-col items-start md:items-center flex-1 px-10">
+				<span className="text-std-20N-150">{user.name}</span>
+				<span className="text-std-22B-150 flex-1 text-right">￥{amount}</span>
+			</p>
 		</div>
 	);
 }
--- app/web/app/features/payments/components/payment-user-list.tsx
+++ app/web/app/features/payments/components/payment-user-list.tsx
@@ -1,7 +1,7 @@
 import { Await, useLoaderData } from "@remix-run/react";
 import { Suspense } from "react";
 
-import { Spinner } from "~/components";
+import { Divider, Spinner } from "~/components";
 
 import type { PaymentUserListLoader } from "../loaders";
 import { PaymentUserCard } from "./payment-user-card";
@@ -15,18 +15,21 @@ export function PaymentUserList() {
 				<Await resolve={paymentUsers}>
 					{({ payments }) =>
 						Array.isArray(payments) && payments.length > 0 ? (
-							<div className="w-full min-h-[60vh] flex flex-col items-center p-4 gap-3">
-								{payments.map((paymentUser) => (
-									<PaymentUserCard
-										key={paymentUser.user.userId}
-										paymentUser={paymentUser}
-									/>
+							<div className="flex flex-col gap-9 py-5">
+								{payments.map((payment) => (
+									<>
+										<PaymentUserCard
+											key={`${payment.user.userId}-card`}
+											paymentUser={payment}
+										/>
+										<Divider key={`${payment.user.userId}-divider`} />
+									</>
 								))}
 							</div>
 						) : (
 							<div className="w-full min-h-[60vh] grid place-content-center">
-								<h2 className="font-semibold text-2xl text-center">
-									返済は存在しません❗️
+								<h2 className="text-std-24N-150 text-2xl text-center">
+									返済は存在しません
 								</h2>
 							</div>
 						)

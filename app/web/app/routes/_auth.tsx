44852b67099db067f4c1f4b398c685a250a3cb2b
rom6621 <3103rom@gmail.com>
2024-11-04 19:07:41 +0900

    feat: SideMenuを共通レイアウトとして表示するように変更

--- app/web/app/routes/_auth.tsx
+++ app/web/app/routes/_auth.tsx
@@ -1,13 +1,24 @@
-import { Outlet } from "@remix-run/react";
-import { Header } from "~/components/Header";
+import { Outlet, Link as RouterLink } from "@remix-run/react";
+
+import { SideMenu } from "~/components";
 
 export { profileLoader as loader } from "~/features/profile/loaders";
 
 export default function Auth() {
 	return (
-		<div className="min-h-screen">
-			<Header />
-			<div className="container py-3">
+		<div className="grid grid-cols-12 justify-center container gap-6">
+			<header className="col-span-12 my-5">
+				<RouterLink to="/" className="text-std-20M-5">
+					Datti
+				</RouterLink>
+				<p className="text-std-16M-7">
+					誰にいくら払ったっけ？を記録するサービス
+				</p>
+			</header>
+			<div className="hidden md:flex col-span-3 flex-col my-10">
+				<SideMenu />
+			</div>
+			<div className="col-span-12 md:col-span-9">
 				<Outlet />
 			</div>
 		</div>

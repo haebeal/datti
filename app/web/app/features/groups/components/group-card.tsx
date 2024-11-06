e6557c6232a7035439bead01850daf988842329b
rom6621 <3103rom@gmail.com>
2024-11-06 19:29:24 +0900

    feat: グループ一覧においてカード全体を押下時に対象グループの詳細画面に遷移するように変更

--- app/web/app/features/groups/components/group-card.tsx
+++ app/web/app/features/groups/components/group-card.tsx
@@ -7,21 +7,22 @@ interface Props {
 
 export function GroupCard({ group }: Props) {
 	return (
-		<div className="flex flex-row gap-5 items-center justify-between py-5 px-3">
+		<Link
+			to={`/groups/${group.groupId}`}
+			className="flex flex-row gap-5 items-center justify-between py-5 px-3"
+		>
 			<span className="text-std-20N-150 pl-3">{group.name}</span>
-			<Link to={`/groups/${group.groupId}`}>
-				<svg
-					role="img"
-					aria-label={`${group.name}を開く`}
-					xmlns="http://www.w3.org/2000/svg"
-					height="45px"
-					width="45px"
-					viewBox="0 -960 960 960"
-					fill="#000000"
-				>
-					<path d="M540-480 356-664l20-20 204 204-204 204-20-20 184-184Z" />
-				</svg>
-			</Link>
-		</div>
+			<svg
+				role="img"
+				aria-label={`${group.name}を開く`}
+				xmlns="http://www.w3.org/2000/svg"
+				height="45px"
+				width="45px"
+				viewBox="0 -960 960 960"
+				fill="#000000"
+			>
+				<path d="M540-480 356-664l20-20 204 204-204 204-20-20 184-184Z" />
+			</svg>
+		</Link>
 	);
 }

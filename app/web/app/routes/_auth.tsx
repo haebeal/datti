691f4f56afe6e264729bb3b088edbb53addb3ea0
rom6621 <3103rom@gmail.com>
2024-11-09 21:06:03 +0900

    feat: モバイル表示時にメニューで表示領域が隠れないよう修正

--- app/web/app/routes/_auth.tsx
+++ app/web/app/routes/_auth.tsx
@@ -6,7 +6,7 @@ export { profileLoader as loader } from "~/features/profile/loaders";
 
 export default function Auth() {
 	return (
-		<div className="grid grid-cols-12 justify-center container gap-6 mb-10">
+		<div className="grid grid-cols-12 justify-center container gap-6">
 			<header className="col-span-12 my-5">
 				<RouterLink to="/" className="text-std-20N-150">
 					Datti
@@ -18,7 +18,7 @@ export default function Auth() {
 			<div className="hidden md:flex col-span-3 flex-col my-10">
 				<SideMenu />
 			</div>
-			<div className="col-span-12 md:col-span-9">
+			<div className="col-span-12 md:col-span-9 mb-20">
 				<Outlet />
 			</div>
 			<MobileMenu />

3727cbd15241f5e5700e0c7f4399adce650c6724
rom6621 <3103rom@gmail.com>
2024-11-09 23:42:03 +0900

    feat: メンバー追加ダイアログのUIを更改

--- app/web/app/routes/_auth.groups_.$groupId.members.tsx
+++ app/web/app/routes/_auth.groups_.$groupId.members.tsx
@@ -1,15 +1,8 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { Outlet, useActionData, useNavigation } from "@remix-run/react";
-import { useEffect } from "react";
+import { Outlet, useActionData } from "@remix-run/react";
+import { useEffect, useRef } from "react";
 
-import { Button } from "~/components/ui/button";
-import {
-	Dialog,
-	DialogContent,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "~/components/ui/dialog";
+import { Button, Dialog, DialogBody } from "~/components";
 import { useToast } from "~/components/ui/use-toast";
 
 import type { AddMemberAction } from "~/features/members/actions";
@@ -23,8 +16,9 @@ export const meta: MetaFunction = () => [
 ];
 
 export default function GroupMembers() {
-	const { state } = useNavigation();
 	const { toast } = useToast();
+
+	const dialogRef = useRef<HTMLDialogElement>(null);
 
 	const actionData = useActionData<AddMemberAction>();
 	useEffect(() => {
@@ -32,29 +26,31 @@ export default function GroupMembers() {
 			toast({
 				title: actionData.message,
 			});
+			dialogRef.current?.close();
 		}
 	}, [actionData, toast]);
 
 	return (
 		<div className="flex flex-col py-3 gap-3">
 			<div className="flex flex-row-reverse items-center justify-items-end">
-				<Dialog>
-					<DialogTrigger asChild>
-						<Button
-							disabled={state !== "idle"}
-							className="bg-sky-500 hover:bg-sky-600 font-semibold"
-						>
-							メンバー追加
-						</Button>
-					</DialogTrigger>
-					<DialogContent>
-						<DialogHeader>
-							<DialogTitle>メンバー追加</DialogTitle>
-						</DialogHeader>
-						<AddMemberForm />
-					</DialogContent>
-				</Dialog>
+				<Button
+					size="md"
+					onClick={() => dialogRef.current?.showModal()}
+					variant="solid-fill"
+				>
+					メンバー追加
+				</Button>
 			</div>
+			<Dialog
+				aria-labelledby="add-member-dialog"
+				className="w-full max-w-[calc(560/16*1rem)]"
+				ref={dialogRef}
+			>
+				<DialogBody>
+					<h2 className="text-std-24N-150">メンバー追加</h2>
+					<AddMemberForm />
+				</DialogBody>
+			</Dialog>
 			<MemberList />
 			<Outlet />
 		</div>

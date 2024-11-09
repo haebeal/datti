c980a329a5637055b6cc89d40ce4e86150c0ab9b
rom6621 <3103rom@gmail.com>
2024-11-10 00:30:12 +0900

    feat: 返済スキーマのエラーメッセージを日本語化

--- app/web/app/features/payments/schemas/index.ts
+++ app/web/app/features/payments/schemas/index.ts
@@ -3,14 +3,32 @@ import type { PaymentCreate, PaymentUpdate } from "~/api/@types";
 import type { ToZod } from "~/lib/toZod";
 
 export const createPaymentSchema = z.object<ToZod<PaymentCreate>>({
-	paidTo: z.string(),
-	paidAt: z.string().datetime(),
-	amount: z.number(),
+	paidTo: z.string({
+		required_error: "必須項目です",
+	}),
+	paidAt: z
+		.string({
+			required_error: "必須項目です",
+		})
+		.datetime(),
+	amount: z.number({
+		required_error: "必須項目です",
+	}),
 });
 
 export const updatePaymentSchema = z.object<ToZod<PaymentUpdate>>({
-	paidTo: z.string(),
-	paidAt: z.string().datetime(),
-	paidBy: z.string().datetime(),
-	amount: z.number(),
+	paidTo: z.string({
+		required_error: "必須項目です",
+	}),
+	paidAt: z
+		.string({
+			required_error: "必須項目です",
+		})
+		.datetime(),
+	paidBy: z.string({
+		required_error: "必須項目です",
+	}),
+	amount: z.number({
+		required_error: "必須項目です",
+	}),
 });

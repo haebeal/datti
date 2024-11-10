7318b406c7fc6d19f395c5f02ce87908150770eb
rom6621 <3103rom@gmail.com>
2024-11-10 10:35:07 +0900

    feat: メンバー追加画面においてSearchUserFormコンポーネントを使用するように修正

--- app/web/app/routes/_auth.groups_.$groupId_.members_.add.tsx
+++ app/web/app/routes/_auth.groups_.$groupId_.members_.add.tsx
@@ -5,7 +5,8 @@ import { useEffect } from "react";
 import { useToast } from "~/components/ui/use-toast";
 
 import type { AddMemberAction } from "~/features/members/actions";
-import { AddMemberForm, AddMemberList } from "~/features/members/components";
+import { AddMemberList } from "~/features/members/components";
+import { SearchUserForm } from "~/features/users/components";
 export { addMemberAction as action } from "~/features/members/actions";
 export { addMemberLoader as loader } from "~/features/members/loaders";
 
@@ -34,7 +35,7 @@ export default function AddMember() {
 			<div className="flex flex-col md:flex-row gap-5 justify-between md:py-5 px-3">
 				<h1 className="text-std-32N-150">メンバー追加</h1>
 			</div>
-			<AddMemberForm />
+			<SearchUserForm />
 			<AddMemberList />
 		</div>
 	);

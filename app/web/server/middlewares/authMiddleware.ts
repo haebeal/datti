f91467713793738af3f10585bea6be4b82d9ef93
rom6621 <3103rom@gmail.com>
2025-02-09 21:41:14 +0900

    feat: アクセストークンの取得に失敗した際、ログインページにリダイレクトさせるように修正

--- app/web/server/middlewares/authMiddleware.ts
+++ app/web/server/middlewares/authMiddleware.ts
@@ -1,21 +1,21 @@
 import { createMiddleware } from "hono/factory";
-import { HTTPException } from "hono/http-exception";
 import type { Env } from "server";
 import { createSupabase } from "server/utils/supabase";
 
 export const authMiddleware = createMiddleware<Env>(async (c, next) => {
 	if (c.req.path !== "/signin") {
 		const supabase = createSupabase(c);
-		await supabase.auth.getUser();
+		const { error: getUserError } = await supabase.auth.getUser();
+		if (getUserError) {
+			return c.redirect("/signin");
+		}
 		const {
 			data: { session },
-			error,
+			error: getSessionError,
 		} = await supabase.auth.getSession();
 		const accessToken = session?.access_token;
-		if (error) {
-			throw new HTTPException(500, {
-				message: "failed to get accessToken",
-			});
+		if (getSessionError || accessToken === undefined) {
+			return c.redirect("/signin");
 		}
 		c.set("accessToken", accessToken);
 	}

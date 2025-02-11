ebc227f8a5cfd0cb814606349c99c2fa157dd6be
rom6621 <3103rom@gmail.com>
2025-02-11 18:20:19 +0900

    feat: グループメンバー取得時のErrorBoundaryを追加

--- app/web/app/routes/_auth+/groups+/$groupId+/members+/index.tsx
+++ app/web/app/routes/_auth+/groups+/$groupId+/members+/index.tsx
@@ -1,4 +1,7 @@
+import { HTTPError } from "@aspida/fetch";
 import type { MetaFunction } from "react-router";
+
+import type { Route } from "./+types/add";
 
 import { MemberList } from "~/features/members/components";
 export { memberListLoader as loader } from "~/features/members/loaders";
@@ -12,6 +15,28 @@ export default function GroupMembers() {
 	return (
 		<div className="flex flex-col py-3 gap-3">
 			<MemberList />
+		</div>
+	);
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+	if (error instanceof HTTPError && error.response.status === 404) {
+		return (
+			<div className="flex flex-col gap-3 pt-32">
+				<h1 className="text-std-45B-140 text-center">404</h1>
+				<h3 className="text-std-22N-150 text-center">
+					グループメンバーの取得に失敗しました
+				</h3>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex flex-col gap-3 pt-32">
+			<h1 className="text-std-45B-140 text-center">500</h1>
+			<h3 className="text-std-22N-150 text-center">
+				不明なエラーが発生しました
+			</h3>
 		</div>
 	);
 }

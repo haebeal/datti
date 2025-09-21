a7c024d425f8050ad1c8efea084d81c988303e9b
nas <vividnasubi@gmail.com>
2025-09-21 23:50:47 +0900

    userを削除しdebtor、payerに持たせる実装に変更

--- apps/backend/internal/domain/payer.go
+++ apps/backend/internal/domain/payer.go
@@ -1,15 +1,50 @@
 package domain
 
+import "github.com/google/uuid"
+
 // 支払い者
 type Payer struct {
-	*User
+	id     uuid.UUID
+	name   string
+	avatar string
+	email  string
 	amount *Amount
 }
 
-func NewPayer(u *User, a *Amount) (*Payer, error) {
-	return &Payer{User: u, amount: a}, nil
+func NewPayer(id uuid.UUID, name string, avatar string, email string, amount *Amount) (*Payer, error) {
+	return &Payer{
+		id:     id,
+		name:   name,
+		avatar: avatar,
+		email:  email,
+		amount: amount,
+	}, nil
+}
+
+func (p *Payer) Equal(c *Payer) bool {
+	return p.id.String() == c.id.String()
+}
+
+func (p *Payer) ID() uuid.UUID {
+	return p.id
+}
+
+func (p *Payer) Name() string {
+	return p.name
+}
+
+func (p *Payer) Avatar() string {
+	return p.avatar
+}
+
+func (p *Payer) Email() string {
+	return p.email
 }
 
 func (p *Payer) Amount() *Amount {
 	return p.amount
+}
+
+type PayerRepository interface {
+	FindByEventID(uuid.UUID) (*[]Payer, error)
 }

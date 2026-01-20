61ee111dbccaeb420f0fcea91a8b759ff33f490b
rom6621 <3103rom@gmail.com>
2026-01-20 23:14:55 +0900

    ECRを追加

--- infra/aws.go
+++ infra/aws.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ecr"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -68,6 +69,47 @@ func createAWSResources(ctx *pulumi.Context) error {
 	})
 	if err != nil {
 		return err
+	}
+
+	// ECR ライフサイクルポリシー
+	// NOTE: コスト削減のためuntaggedになってから一日経ったイメージを全て削除
+	ecrLifecyclePolicy := `{
+		"rules": [
+			{
+				"rulePriority": 1,
+				"description": "Delete untagged images after 1 day",
+				"selection": {
+					"tagStatus": "untagged",
+					"countType": "sinceImagePushed",
+					"countUnit": "days",
+					"countNumber": 1
+				},
+				"action": {
+					"type": "expire"
+				}
+			}
+		]
+	}`
+
+	// ECRリポジトリ
+	ecrRepos := []string{"datti-backend", "datti-frontend"}
+	for _, name := range ecrRepos {
+		repo, err := ecr.NewRepository(ctx, name, &ecr.RepositoryArgs{
+			Name:               pulumi.String(name),
+			ImageTagMutability: pulumi.String("MUTABLE"),
+			ForceDelete:        pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = ecr.NewLifecyclePolicy(ctx, name+"-lifecycle", &ecr.LifecyclePolicyArgs{
+			Repository: repo.Name,
+			Policy:     pulumi.String(ecrLifecyclePolicy),
+		})
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil

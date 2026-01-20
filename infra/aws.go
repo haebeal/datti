b947456bed96c0689d79780069d3bba0f1213498
rom6621 <3103rom@gmail.com>
2026-01-21 00:12:53 +0900

    ECSクラスターとECSインスタンスの作成

--- infra/aws.go
+++ infra/aws.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ecr"
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ecs"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ssm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -134,6 +138,15 @@ func createAWSResources(ctx *pulumi.Context) error {
 		return err
 	}
 
+	// ECS Instance(EC2) プロファイル
+	instanceProfile, err := iam.NewInstanceProfile(ctx, "datti-ecs-instance-profile", &iam.InstanceProfileArgs{
+		Name: pulumi.String("datti-ecs-instance-profile"),
+		Role: instanceRole.Name,
+	})
+	if err != nil {
+		return err
+	}
+
 	// ECS InstanceロールにECSマネージドポリシーをアタッチ
 	_, err = iam.NewRolePolicyAttachment(ctx, "datti-ecs-instance-policy", &iam.RolePolicyAttachmentArgs{
 		Role:      instanceRole.Name,
@@ -181,6 +194,71 @@ func createAWSResources(ctx *pulumi.Context) error {
 	if err != nil {
 		return err
 	}
+
+	// ECSクラスター
+	cluster, err := ecs.NewCluster(ctx, "datti-cluster", &ecs.ClusterArgs{
+		Name: pulumi.String("datti-cluster"),
+	})
+	if err != nil {
+		return err
+	}
+
+	// セキュリティグループ
+	// NOTE: アウトバウンドのみ許可するように
+	sg, err := ec2.NewSecurityGroup(ctx, "datti-ecs-sg", &ec2.SecurityGroupArgs{
+		Name:        pulumi.String("datti-ecs-sg"),
+		Description: pulumi.String("Security group for Datti ECS instances"),
+		VpcId:       vpc.ID(),
+		Egress: ec2.SecurityGroupEgressArray{
+			&ec2.SecurityGroupEgressArgs{
+				Protocol:   pulumi.String("-1"), // TODO: 全てのプロトコルになっているため、Cloudflare Tunnnelに必要なプロトコルに絞る
+				FromPort:   pulumi.Int(0),
+				ToPort:     pulumi.Int(0),
+				CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+			},
+		},
+		Tags: pulumi.StringMap{
+			"Name": pulumi.String("datti-ecs-sg"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	// ECS最適化AMIを取得
+	// NOTE: AWSはSSM Parameter Storeに最新AMIのIDを後悔している
+	ecsAmi, err := ssm.LookupParameter(ctx, &ssm.LookupParameterArgs{
+		Name: "/aws/service/ecs/optimized-ami/amazon-linux-2023/arm64/recommended/image_id",
+	})
+	if err != nil {
+		return err
+	}
+
+	// クラスタ名が設定される
+	// TODO: ここの理解をしたい
+	userData := cluster.Name.ApplyT(func(name string) string {
+		return fmt.Sprintf(`#!/bin/bash
+echo ECS_CLUSTER=%s >> /etc/ecs/ecs.config
+`, name)
+	}).(pulumi.StringOutput)
+
+	// ECS Instanceとして使用するEC2(t4g.small)を作成
+	_, err = ec2.NewInstance(ctx, "datti-ecs-instance", &ec2.InstanceArgs{
+		Ami:                 pulumi.String(ecsAmi.Value),
+		InstanceType:        pulumi.String("t4g.small"),
+		SubnetId:            subnet.ID(),
+		VpcSecurityGroupIds: pulumi.StringArray{sg.ID()},
+		IamInstanceProfile:  instanceProfile.Name,
+		UserData:            userData,
+		Tags: pulumi.StringMap{
+			"Name": pulumi.String("datti-ecs-instance"),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	ctx.Export("ecsClusterName", cluster.Name)
 
 	return nil
 }

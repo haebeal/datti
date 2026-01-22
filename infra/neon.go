c5042ca36b61e87662d8bc2be9a4643d9c9add90
rom6621 <3103rom@gmail.com>
2026-01-22 20:20:19 +0900

    neonプロジェクトを新規で作成するように

--- infra/neon.go
+++ infra/neon.go
@@ -1,45 +1,33 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ssm"
 	"github.com/pulumi/pulumi-terraform-provider/sdks/go/neon/neon"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func createNeonResources(ctx *pulumi.Context) error {
-	_, err := neon.NewProject(ctx, "datti-neon-project", &neon.ProjectArgs{
+	project, err := neon.NewProject(ctx, "datti-neon", &neon.ProjectArgs{
+		Name:                    pulumi.String("datti"),
+		OrgId:                   pulumi.String("org-frosty-mountain-46580130"),
+		RegionId:                pulumi.String("aws-ap-southeast-1"),
+		PgVersion:               pulumi.Float64(17),
+		HistoryRetentionSeconds: pulumi.Float64(21600), // 無料枠上限: 6時間
 		Branch: &neon.ProjectBranchArgs{
-			DatabaseName: pulumi.String("neondb"),
 			Name:         pulumi.String("prod"),
+			DatabaseName: pulumi.String("neondb"),
 			RoleName:     pulumi.String("neondb_owner"),
 		},
-		ComputeProvisioner: pulumi.String("k8s-neonvm"),
-		DefaultEndpointSettings: &neon.ProjectDefaultEndpointSettingsArgs{
-			AutoscalingLimitMaxCu: pulumi.Float64(2),
-			AutoscalingLimitMinCu: pulumi.Float64(0.25),
-		},
-		HistoryRetentionSeconds: pulumi.Float64(86400),
-		MaintenanceWindow: &neon.ProjectMaintenanceWindowArgs{
-			EndTime:   pulumi.String("19:00"),
-			StartTime: pulumi.String("18:00"),
-			Weekdays: pulumi.Float64Array{
-				pulumi.Float64(2),
-			},
-		},
-		Name:          pulumi.String("Datti-dev"),
-		OrgId:         pulumi.String("org-frosty-mountain-46580130"),
-		PgVersion:     pulumi.Float64(17),
-		RegionId:      pulumi.String("aws-ap-southeast-1"),
-		StorePassword: pulumi.String("yes"),
-	}, pulumi.Protect(true))
+	})
 	if err != nil {
 		return err
 	}
 
-	_, err = neon.NewBranch(ctx, "neon-branch-dev", &neon.BranchArgs{
-		Name:      pulumi.String("dev"),
-		ParentId:  pulumi.String("br-polished-bonus-a10yscty"),
-		ProjectId: pulumi.String("quiet-wildflower-78644609"),
-	}, pulumi.Protect(true))
+	_, err = ssm.NewParameter(ctx, "datti-dev-dsn", &ssm.ParameterArgs{
+		Name:  pulumi.String("/datti/dev/backend/DSN"),
+		Type:  pulumi.String("SecureString"),
+		Value: project.ConnectionUri,
+	})
 	if err != nil {
 		return err
 	}
